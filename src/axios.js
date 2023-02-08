f30ed574f114aeb73f32674876df0a5a6363a9a8
Aybek <idevkun@gmail.com>
2023-02-08 18:09:50 +0500

    add some changes

--- src/axios.js
+++ src/axios.js
@@ -8,6 +8,7 @@ const $authHost = axios.create({
   baseURL: import.meta.env.VITE_APP_API_URL,
 });
 
+
 $host.interceptors.request.use((config) => {
   config.headers.authorization = `Bearer ${window.localStorage.getItem("token")}`;
   return config;
